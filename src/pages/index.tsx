222c85786fb25e24f42fe473f0df5d9281a81d94
nasuka7 <n4suka7@gmail.com>
2021-04-21 22:23:03 +0900

    Add indexpage

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Headbar from '../components/Headbar';
+
+const Index = (): JSX.Element => {
+  return (
+    <div>
+      <Headbar />
+      <p>Hello Index</p>
+    </div>
+  )
+};
+
+export default Index;
