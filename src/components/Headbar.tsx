fa6e019514e382a0338754e1bfa9665e6d4d4ef9
nasuka7 <n4suka7@gmail.com>
2021-04-26 23:57:49 +0900

    Add header.tsx

--- src/components/Headbar.tsx
+++ src/components/Headbar.tsx
@@ -3,25 +3,24 @@ import { FunctionComponent } from 'react';
 
 const Headbar: FunctionComponent = () => {
   return (
-    <div>
-      <div className="font-bold text-3xl ml-12 mt-12 border-b-2 border-double border-gray-800 w-24 pb-2">
-        KKBlog
-      </div>
-      <div className="flex justify-center pt-20 space-x-32 md:space-x-12 text-gray-700">
-        <div className="hover:text-blue-700 active:bg-gray-300 border-l-2">
-          <Link href="/blog">Blog</Link>
-        </div>
-        <div>
-          <Link href="/blog">Blog</Link>
-        </div>
+    <div className="max-w-7xl flex mx-auto py-5 border-b border-gray-300">
+      <div className="flex justify-between">
         <div className="">
-          <Link href="/">Home</Link>
+          <Link href="/">KKBlog</Link>
         </div>
-        <div>
-          <Link href="/profile">Profile</Link>
-        </div>
-        <div>
-          <Link href="/contact">Contact</Link>
+        <div className="flex flex-row"> 
+          <div className="pr-5">
+            <Link href="/blog">Blog</Link>
+          </div>
+          <div className="pr-5">
+            <Link href="/">Home</Link>
+          </div>
+          <div className="pr-5">
+            <Link href="/profile">Profile</Link>
+          </div>
+          <div className="pr-5">
+            <Link href="/contact">Contact</Link>
+          </div>
         </div>
       </div>
     </div>
