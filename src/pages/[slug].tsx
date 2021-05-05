7442dd9ad6a3c5da365a970aa365feb8e0e4830b
nasuka7 <n4suka7@gmail.com>
2021-05-05 14:51:19 +0900

    fix error blog

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,8 +1,7 @@
 import { format, parseISO } from 'date-fns';
-import { getAllPosts } from '../../lib/data';
+import { getAllPosts } from '../lib/data';
 import renderToString from 'next-mdx-remote/render-to-string';
 import hydrate from 'next-mdx-remote/hydrate';
-import { GetStaticPaths, GetStaticProps } from 'next'
 
 export default function BlogPage({ title, date, content }) {
   const hydratedContent = hydrate(content);
