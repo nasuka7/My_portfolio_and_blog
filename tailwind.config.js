52ecee2863d7a21f814dc0c204fbd5c75335bbc2
nasuka7 <n4suka7@gmail.com>
2021-05-07 22:31:39 +0900

    fix error vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,24 @@
 module.exports = {
-  purge: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
-  darkMode: "class",
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
         dark: {
-          DEFAULT: "#010101",
-          100: "#0a0b0e",
-          200: "#16181d",
-          500: "#0f1115",
-          700: "#202125",
+          DEFAULT: '#010101',
+          100: '#0a0b0e',
+          200: '#16181d',
+          500: '#0f1115',
+          700: '#202125',
         },
       },
     },
   },
   variants: {
-    extend: {
-      textColor: ['active'],
-    },
+    extend: {},
   },
   plugins: [],
 };
